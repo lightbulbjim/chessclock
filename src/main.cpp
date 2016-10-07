8589faa842adff2361c662aca0761b0c3e816855
Chris Rendle-Short <chris@killred.net>
2016-10-07 15:04:14 +1100

    Basic multi display test working.

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,34 @@
  * Simple blinky thing.
  */
 #include "Arduino.h"
+#include "LiquidCrystal.h"
+
+LiquidCrystal leftDisplay(6, 7, 2, 3, 4, 5);
+LiquidCrystal rightDisplay(6, 8, 2, 3, 4, 5);
 
 void setup()
 {
-  pinMode(17, OUTPUT);
+	leftDisplay.begin(20,4);
+	rightDisplay.begin(20,4);
+}
+
+void lcdTest()
+{
+	int i;
+	for ( i=1; i<=10; i++ ) {
+		leftDisplay.clear();
+		leftDisplay.print("Testing left ");
+		leftDisplay.print(i);
+
+		rightDisplay.clear();
+		rightDisplay.print("Testing right ");
+		rightDisplay.print(i);
+
+		delay(1000);
+	}
 }
 
 void loop()
 {
-  digitalWrite(17, LOW);
-  delay(100);
-  digitalWrite(17, HIGH);
-  delay(100);
-  digitalWrite(17, LOW);
-  delay(100);
-  digitalWrite(17, HIGH);
-
-  delay(1000);
+	lcdTest();
 }
