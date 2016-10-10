d95add102f11809b3008a67ea46230dc6cc186b9
Chris Rendle-Short <chris@killred.net>
2016-10-10 16:33:59 +1100

    Name leftClock a bit more descriptively.

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 LiquidCrystal leftDisplay(6, 7, 2, 3, 4, 5);
 LiquidCrystal rightDisplay(6, 8, 2, 3, 4, 5);
 
-BigTime left(&leftDisplay);
+BigTime leftClock(&leftDisplay);
 
 void setup()
 {
@@ -23,13 +23,13 @@ void lcdTest()
 {
 	int i;
 	for ( i=0; i<=9; i++ ) {
-		left.printDigit(i, 0);
-		left.printColon(3);
-		left.printDigit(i, 4);
-		left.printDigit(i, 7);
-		left.printColon(10);
-		left.printDigit(i, 11);
-		left.printDigit(i, 14);
+		leftClock.printDigit(i, 0);
+		leftClock.printColon(3);
+		leftClock.printDigit(i, 4);
+		leftClock.printDigit(i, 7);
+		leftClock.printColon(10);
+		leftClock.printDigit(i, 11);
+		leftClock.printDigit(i, 14);
 
 		rightDisplay.clear();
 		rightDisplay.print("Testing right ");
