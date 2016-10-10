5af90f3df74b13cb17c0fcf475307fd14c7b385b
Chris Rendle-Short <chris@killred.net>
2016-10-10 16:45:56 +1100

    Expand LCD test to include both displays.

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ LiquidCrystal leftDisplay(6, 7, 2, 3, 4, 5);
 LiquidCrystal rightDisplay(6, 8, 2, 3, 4, 5);
 
 BigTime leftClock(&leftDisplay);
+BigTime rightClock(&rightDisplay);
+
 
 void setup()
 {
@@ -19,10 +21,16 @@ void setup()
 	rightDisplay.clear();
 }
 
+
 void lcdTest()
 {
 	int i;
 	for ( i=0; i<=9; i++ ) {
+		leftDisplay.clear();
+		leftDisplay.setCursor(0,3);
+		leftDisplay.print("Testing left ");
+		leftDisplay.print(i);
+
 		leftClock.printDigit(i, 0);
 		leftClock.printColon(3);
 		leftClock.printDigit(i, 4);
@@ -32,12 +40,22 @@ void lcdTest()
 		leftClock.printDigit(i, 14);
 
 		rightDisplay.clear();
+		rightDisplay.setCursor(0,3);
 		rightDisplay.print("Testing right ");
 		rightDisplay.print(i);
+
+		rightClock.printDigit(i, 0);
+		rightClock.printColon(3);
+		rightClock.printDigit(i, 4);
+		rightClock.printDigit(i, 7);
+		rightClock.printColon(10);
+		rightClock.printDigit(i, 11);
+		rightClock.printDigit(i, 14);
 
 		delay(2000);
 	}
 }
+
 
 void loop()
 {
