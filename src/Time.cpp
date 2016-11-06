6e449a8075c77e163915f5b96ca382942a51168e
Chris Rendle-Short <chris@killred.net>
2016-11-07 07:57:28 +1100

    Add time format helper methods.

--- src/Time.cpp
+++ src/Time.cpp
@@ -9,3 +9,15 @@ bool Time::isZero()
 {
 	return (this->hours == 0) || (this->minutes == 0) || (this->seconds == 0);
 }
+
+
+unsigned int Time::inMinutes()
+{
+	return (this->hours * 60) + this->minutes;
+}
+
+
+unsigned long Time::inSeconds()
+{
+	return (this->inMinutes() * 60) + this->seconds;
+}
