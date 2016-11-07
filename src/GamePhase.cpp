a99b34095d1697483b5a1d6edb3c5a743f284dc1
Chris Rendle-Short <chris@killred.net>
2016-11-08 08:42:56 +1100

    Handle seconds printing in GamePhase::getDescription(). Probably won't ever be used in a real situation but useful for testing.

--- src/GamePhase.cpp
+++ src/GamePhase.cpp
@@ -9,9 +9,17 @@ void GamePhase::getDescription(char* buffer)
 {
 	static const byte length = 10;
 
-	if (this->moveLimit > 0) {
-		snprintf(buffer, length + 1, "%d/%d", this->moveLimit, this->time.inMinutes());
+	if (this->time.hours == 0 && this->time.minutes == 0) {
+		if (this->moveLimit > 0) {
+			snprintf(buffer, length + 1, "%d/%ds", this->moveLimit, this->time.inSeconds());
+		} else {
+		    snprintf(buffer, length + 1, "G/%ds", this->time.inSeconds());
+		}
 	} else {
-	    snprintf(buffer, length + 1, "G/%d", this->time.inMinutes());
+		if (this->moveLimit > 0) {
+			snprintf(buffer, length + 1, "%d/%d", this->moveLimit, this->time.inMinutes());
+		} else {
+		    snprintf(buffer, length + 1, "G/%d", this->time.inMinutes());
+		}
 	}
 }
