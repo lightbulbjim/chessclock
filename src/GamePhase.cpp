e8182fb6579c288a70556ef10f91482bab39f0b2
Chris Rendle-Short <chris@killred.net>
2016-11-08 20:44:58 +1100

    Clean up format strings in GamePhase::getDescription().

--- src/GamePhase.cpp
+++ src/GamePhase.cpp
@@ -11,15 +11,15 @@ void GamePhase::getDescription(char* buffer)
 
 	if (this->time.hours == 0 && this->time.minutes == 0) {
 		if (this->moveLimit > 0) {
-			snprintf(buffer, length + 1, "%d/%ds", this->moveLimit, this->time.inSeconds());
+			snprintf(buffer, length + 1, "%d/%lus", this->moveLimit, this->time.inSeconds());
 		} else {
-		    snprintf(buffer, length + 1, "G/%ds", this->time.inSeconds());
+		    snprintf(buffer, length + 1, "G/%lus", this->time.inSeconds());
 		}
 	} else {
 		if (this->moveLimit > 0) {
-			snprintf(buffer, length + 1, "%d/%dm", this->moveLimit, this->time.inMinutes());
+			snprintf(buffer, length + 1, "%d/%um", this->moveLimit, this->time.inMinutes());
 		} else {
-		    snprintf(buffer, length + 1, "G/%dm", this->time.inMinutes());
+		    snprintf(buffer, length + 1, "G/%um", this->time.inMinutes());
 		}
 	}
 }
